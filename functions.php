f37455e855198e4d34ad532312170acc5f02f049
sazon-mahmud <developer.sazon@gmail.com>
2023-08-29 17:18:33 +0600

    Create Custom Menu

--- functions.php
+++ functions.php
@@ -5,6 +5,16 @@
         wp_enqueue_scripts( 'bootstrap-js', bloginfo('template_directory') . 'js/mdb.min.js', array('query'), '6.4.1', true);
         
     }
-    add_action('wp_enqueue_scripts', 'load_scripts');
- 
-?>  
+    add_action('wp_enqueue_scripts', 'load_scripts'); 
+
+
+// Create Custom Menus
+register_nav_menus(
+    array(
+        'main_menu' => 'Top Menu',
+        'side_menu' => 'Side Menu'
+
+    )
+)
+
+?>
