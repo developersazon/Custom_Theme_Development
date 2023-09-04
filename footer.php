ea3c8dfd2dde479f1381a37ae18e60953603de0c
sazon-mahmud <developer.sazon@gmail.com>
2023-09-04 13:03:00 +0600

    Added new footer for my theme

--- footer.php
+++ footer.php
@@ -1,55 +1,125 @@
-   <!--Footer-->
-   <footer class="bg-light text-lg-start">
-      <hr class="m-0" />
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-light text-muted fixed-bottom">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between border-bottom px-3">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
 
-      <div class="text-center py-4 align-items-center">
-        <p>Follow MDB on social media</p>
-        <a
-          href="https://www.youtube.com/channel/UC5CF7mLQZhvx8O5GODZAhdA"
-          class="btn btn-primary m-1"
-          role="button"
-          rel="nofollow"
-          target="_blank"
-        >
-          <i class="fab fa-youtube"></i>
-        </a>
-        <a
-          href="https://www.facebook.com/mdbootstrap"
-          class="btn btn-primary m-1"
-          role="button"
-          rel="nofollow"
-          target="_blank"
-        >
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
           <i class="fab fa-facebook-f"></i>
         </a>
-        <a
-          href="https://twitter.com/MDBootstrap"
-          class="btn btn-primary m-1"
-          role="button"
-          rel="nofollow"
-          target="_blank"
-        >
+        <a href="" class="me-4 text-reset">
           <i class="fab fa-twitter"></i>
         </a>
-        <a
-          href="https://github.com/mdbootstrap/mdb-ui-kit"
-          class="btn btn-primary m-1"
-          role="button"
-          rel="nofollow"
-          target="_blank"
-        >
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
           <i class="fab fa-github"></i>
         </a>
       </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
 
-      <!-- Copyright -->
-      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © 2020 Copyright:
-        <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Learn With Sazon
+            </h6>
+            <p>
+              Here you can use rows and columns to organize your footer content. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Products
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Angular</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">React</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Vue</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Laravel</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Useful links
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Pricing</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Settings</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Orders</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@example.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
       </div>
-      <!-- Copyright -->
-    </footer>
-    <!--Footer-->
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-1" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2023 Copyright:
+      <a class="text-reset fw-bold" href="https://portfolio.sazonmahmud.net">LWSAZON</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
     <!-- MDB -->
     <script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/mdb.min.js"></script>
     <!-- Custom scripts -->
