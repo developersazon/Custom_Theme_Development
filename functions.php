a8ef5e8a4deabb9d308b4a0c38a6656191f357b6
sazon-mahmud <developer.sazon@gmail.com>
2023-09-07 17:00:25 +0600

    In this functions page, I added two functions, the first is custom-header & the other is post-thumbnails functions.

--- functions.php
+++ functions.php
@@ -1,19 +1,26 @@
 <?php
 
-    function load_scripts(){
-        wp_enqueue_style( 'bootstrap-css', bloginfo('template_directory') . 'css/mdb.min.css', array(), '6.4.1', 'all');
-        wp_enqueue_scripts( 'bootstrap-js', bloginfo('template_directory') . 'js/mdb.min.js', array('query'), '6.4.1', true);
+    // function load_scripts(){
+    //     wp_enqueue_style( 'bootstrap-css', bloginfo('template_directory') . 'css/mdb.min.css', array(), '6.4.1', 'all');
+    //     wp_enqueue_scripts( 'bootstrap-js', bloginfo('template_directory') . 'js/mdb.min.js', array('query'), '6.4.1', true);
         
-    }
-    add_action('wp_enqueue_scripts', 'load_scripts'); 
+    // }
+    // add_action('wp_enqueue_scripts', 'load_scripts'); 
 
 
-// Create Custom Menus
+// custom menu start here
 register_nav_menus(
     array(
         'main_menu' => 'Primary Menu',
 
     )
-)
+    );
+//custom menu end here
 
+
+//custom header function start here
+add_theme_support('custom-header');
+
+//create custom thumbnail for post page
+add_theme_support('post-thumbnails');
 ?>
