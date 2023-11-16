105b3e8c8f7513e381d8b22ba57341c75c5941f4
Sazon Mahmud <developer.sazon@gmail.com>
2023-11-16 17:41:49 +0600

    Changed the 404 webpage design as the requirement. I hope if you see my 404 webpage, you must lik it.

--- 404.php
+++ 404.php
@@ -12,43 +12,35 @@
       .four_zero_four_bg{
       
       background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
-          height: 500px;
+          height: 720px;
           background-position: center;
           background-repeat: no-repeat;
           background-size: cover;
       }
       
-      
       .four_zero_four_bg h1{
       font-size:80px;
       }
       
-        .four_zero_four_bg h3{
+     .four_zero_four_bg h3{
             font-size:80px;
-            }
-            
-        .link_404{			 
-          padding: 10px 20px;
-          display: inline-block;
-        }
-        .contant_box_404{ margin-top:-50px;}
+      }
+      .note {
+            margin-top: -150px;
+      }
 </style>
 
 <!-- 404 Section start here -->
-<section class="page_404 p-5" style="background-color:EFEFEF;">
+<section class="page_404 p-5">
           <div class="container">
               <div class="row">	
-                    <div class="col-sm-12">
-                        <div class="col-sm-10 col-sm-offset-1  text-center">
-                            <div class="four_zero_four_bg">
-                              <h1 class="text-center h2">404</h1>
-                            </div>
-                            <div class="contant_box_404">
-                              <h3 class="h3"> Look like you're lost</h3>
-                              <p class="h4">Sorry, but nothing matched your search terms. Please try again with some different keywords.</p>
-                              <a href="<?php echo site_url(); ?>" class="link_404 rounded-pill btn-outline-primary mt-4">Go to Home</a>
-                            </div>
-                        </div>
+                    <div class="col-sm-12 col-sm-offset-1">
+                          <div class="four_zero_four_bg">
+                               <p class="h1 text-primary text-center" style="font-size:80px; font-family: 'Kenia', sans-serif;">Error 404 !</p>
+                          </div>
+                          <div class="note note-success p-3 mx-5">
+                              <p class="h3"><span style="color:blue;"> Look like you're lost! </span> Sorry, but nothing matched your search terms.Please try again with some different keywords. Click here <a href="<?php echo site_url(); ?>" class="btn rounded-pill btn-outline text-primary">Go to HomePage</a></p>
+                          </div>
                     </div>
               </div>
           </div>
