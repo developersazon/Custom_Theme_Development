7c06732fea9f2b8ceae70701c56b78dc2102805e
Sazon Mahmud <developer.sazon@gmail.com>
2023-12-05 18:09:15 +0600

    Adding new functions for the theme

--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 
    function stylesheet_files(){
       wp_enqueue_style('main_css_file', get_stylesheet_directory_uri() . '/css/style.css');
-      wp_enqueue_style('main_bootstrap_file', get_stylesheet_directory_uri(). '/css/mdb.min.css');
-      wp_enqueue_style('font', get_stylesheet_directory_uri(). 'https://use.fontawesome.com/releases/v5.11.2/css/all.css');
+      wp_enqueue_style('bootstrap_js_file', get_stylesheet_directory_uri() . '/css/mdb.min.css');
+      wp_enqueue_style('google-font', get_stylesheet_directory_uri(). '<link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Kenia&family=Roboto+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,400;1,500&display=swap" rel="stylesheet">');
    }
    add_action('wp_enqueue_scripts', 'stylesheet_files');
 
