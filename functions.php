768f6452f03286329ed92ce2a574f53d47f5f68b
Sazon Mahmud <developer.sazon@gmail.com>
2023-12-06 13:21:43 +0600

    Add new function for dynamic the website title & Tag

--- functions.php
+++ functions.php
@@ -3,11 +3,17 @@
    function stylesheet_files(){
       wp_enqueue_style('main_css_file', get_stylesheet_directory_uri() . '/css/style.css');
       wp_enqueue_style('bootstrap_js_file', get_stylesheet_directory_uri() . '/css/mdb.min.css');
-      wp_enqueue_style('google-font', get_stylesheet_directory_uri(). '<link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Fugaz+One&family=Kenia&family=Roboto+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,400;1,500&display=swap" rel="stylesheet">');
+      wp_enqueue_style('font-owesome', get_stylesheet_directory_uri(). 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css');
    }
    add_action('wp_enqueue_scripts', 'stylesheet_files');
+
+
+// theme title & tag setup function start here
+   function website_titletag() {
+      add_theme_support ('title-tag');
+   }
+   add_action('after_setup_theme', 'website_titletag');
+// theme title & tag setup function edn here
 
 ?>
 
