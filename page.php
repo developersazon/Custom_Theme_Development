a4af4feb8168122554d1c82cd6429d2a967f45d4
Sazon Mahmud <developer.sazon@gmail.com>
2023-12-07 16:23:42 +0600

    Added new section for the pages

--- page.php
+++ page.php
@@ -3,14 +3,17 @@
       include_once 'banner.php';
 ?>
 
+ 
+ <?php while(have_posts()){ 
+      the_post();
+  ?>
    <div id="section">
     <div class="container">
-      <div class="col"> 
-         <h2 class="my-5">Hello World</h2>
-      </div>
+      <div class="col my-5"> 
+           <h1><?php bloginfo('home'); ?></h1>
     </div>
    </div>
-  
+   <?php } ?>
  <!-- page banner section end here -->
 
 <?php get_footer(); ?>
