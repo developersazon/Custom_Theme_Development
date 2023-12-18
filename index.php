78095389c9ab07bcfe7375f5393e0929566aaad6
Sazon Mahmud <developer.sazon@gmail.com>
2023-12-18 15:57:27 +0600

    Remove some content and also change the colum style to Row

--- index.php
+++ index.php
@@ -10,15 +10,15 @@
         <p class="h2 py-2">Our Latest Blogs</p>
         <div class="row">
             <!-- left side content start here-->
-            <div class="col-md-8 col-lg-8 d-flex">
-                <div class="col d-flex">
+            <div class="col-md-8 col-lg-8">
+                <div class="col">
                         <!-- card 1 start here -->
-                        <?php while(have_posts() ) { 
+                        <?php while(have_posts()) { 
                             
                             the_post();
                             $banner_image = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
                         ?>
-                        <div class="card m-3">
+                        <div class="card mb-4">
                             <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                                 <img src="<?php echo $banner_image[0]; ?>" class="img-fluid"/>
                                 <a href="<?php the_permalink(); ?>">
@@ -50,8 +50,8 @@
             <!-- Right side content end here -->
 
             <!-- For pagination  -->
-            <P><?php wp_pagenavi(); ?></p>
-        </div>
+            <h4><?php echo paginate_links(); ?></h4>
+        </div>`
     </div>
 </div>
 <!-- Blog Section end here -->
