a689a7129ebc36baa0e21eb1997738713a54ec3d
Sazon Mahmud <developer.sazon@gmail.com>
2023-12-18 13:01:33 +0600

    Add new pages in the footer section Which is used for Company overview page. All these page are display dynamically. We can access and any kind of changes in WordPress Dashboard

--- footer.php
+++ footer.php
@@ -55,20 +55,9 @@
           <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
             <!-- Links -->
             <h6 class="text-uppercase fw-bold mb-4">
-              Products
+              Our Company
             </h6>
-            <p>
-              <a href="#!" class="text-reset">Angular</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">React</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Vue</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Laravel</a>
-            </p>
+            <?php wp_nav_menu( array( 'theme_location' => 'footer_one',)) ?>
           </div>
           <!-- Grid column -->
 
@@ -78,7 +67,7 @@
             <h6 class="text-uppercase fw-bold mb-4">
               Useful links
             </h6>
-              <?php wp_nav_menu( array( 'theme_location' => 'footer_one',)) ?>
+              <?php wp_nav_menu( array( 'theme_location' => 'footer_two',)) ?>
           </div>
           <!-- Grid column -->
 
